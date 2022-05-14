09209d59dda42157470e4bef51708edbc95f7413
m10maeda <mio.maeda.kari@gmail.com>
2022-05-14 22:46:38 +0900

    feat(backend): use presentation module

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,24 @@
+import { PresentationModule } from '@itddd/backend/presentation';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      debug: process.env.NODE_ENV === 'development',
+      playground: process.env.NODE_ENV === 'development',
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      context: ({ req }) => ({ req }),
+    }),
+    PresentationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
