efda69f5c80d6c0956da015a6f6e71f1c566ebc8
m10maeda <mio.maeda.kari@gmail.com>
2023-07-11 22:24:56 +0900

    chore: setup openapi

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,19 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Backend API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT ?? 3333;
 
   await app.listen(port);
