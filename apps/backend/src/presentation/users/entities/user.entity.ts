f2da7d78f73054a4bd4390fcecdbc0cc464d727d
m10maeda <mio.maeda.kari@gmail.com>
2023-07-16 22:08:14 +0900

    docs(backend): make it explicitly to using api response value type

--- apps/backend/src/presentation/users/entities/user.entity.ts
+++ apps/backend/src/presentation/users/entities/user.entity.ts
@@ -12,7 +12,7 @@ export class User {
   @ApiProperty({ example: 'Alice' })
   public readonly name: string;
 
-  @ApiProperty({ example: 'Normal' })
+  @ApiProperty({ example: UserType.Normal, enum: UserType })
   public readonly type: UserType;
 
   public constructor(id: string, name: string, type: UserType) {
