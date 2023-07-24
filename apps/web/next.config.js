5d700d34fda32a696c552f360c6bad8c511dc9ff
m10maeda <mio.maeda.kari@gmail.com>
2023-07-24 21:59:48 +0900

    feat(web): add graphql api client

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ['@itddd/ui'],
+  async rewrites() {
+    return [
+      {
+        source: '/api/graphql',
+        destination: 'http://localhost:3334/graphql',
+      },
+    ];
+  },
 };
