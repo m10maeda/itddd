ed1b75282542a475e8058146091c9a6ddb9b8c96
m10maeda <mio.maeda.kari@gmail.com>
2023-12-15 00:29:21 +0900

    feat(bff): make available class validator

--- apps/bff/src/shared/dto/page-info.args.ts
+++ apps/bff/src/shared/dto/page-info.args.ts
@@ -1,10 +1,15 @@
 import { ArgsType, Field, Int } from '@nestjs/graphql';
+import { IsInt, IsPositive } from 'class-validator';
 
 @ArgsType()
 export class PageInfoArgs {
+  @IsInt()
+  @IsPositive()
   @Field(() => Int, { nullable: true, defaultValue: 1 })
   public readonly page: number;
 
+  @IsInt()
+  @IsPositive()
   @Field(() => Int, { nullable: true, defaultValue: 10 })
   public readonly size: number;
 
