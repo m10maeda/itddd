8e2806047fbc405cbb1634fccc6a0f342aba047e
m10maeda <mio.maeda.kari@gmail.com>
2023-12-15 00:46:35 +0900

    refactor(bff): make not to use service

--- apps/bff/src/users/users.module.ts
+++ apps/bff/src/users/users.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 
 import { UsersResolver } from './users.resolver';
-import { UsersService } from './users.service';
 import { Configuration, UsersApi } from '../../lib/backend-adapter/v1.0';
 
 @Module({
@@ -15,7 +14,6 @@ import { Configuration, UsersApi } from '../../lib/backend-adapter/v1.0';
         }),
       ),
     },
-    UsersService,
     UsersResolver,
   ],
 })
