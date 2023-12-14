8d966eba6448008b0915cba735f179d2aaa88641
m10maeda <mio.maeda.kari@gmail.com>
2023-12-15 00:23:26 +0900

    feat(backend): make available transform pipe

--- apps/backend/src/presentation/presentation.module.ts
+++ apps/backend/src/presentation/presentation.module.ts
@@ -1,8 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 
 import { UsersPresentationModule } from './users';
 
 @Module({
   imports: [UsersPresentationModule],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        transformOptions: { enableImplicitConversion: true },
+      }),
+    },
+  ],
 })
 export class PresentationModule {}
