5e5a792a0db4ac07aa56e35368906a4c5d6092ba
m10maeda <mio.maeda.kari@gmail.com>
2024-12-11 19:33:16 +0900

    feat(profiles): inject dependencies

--- apps/profiles/src/app.module.ts
+++ apps/profiles/src/app.module.ts
@@ -1,11 +1,52 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe, type Provider } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApplicationService } from './application';
+import {
+  type IProfileEventPublisher,
+  type IProfileFactory,
+  type IProfileRepository,
+} from './domain/models';
+import { ProfileExistenceService } from './domain/services';
+import { ProfileEventBus } from './infrastructure/event-bus';
+import {
+  InfrastructureModule,
+  PROFILE_FACTORY,
+  PROFILE_REPOSITORY,
+} from './infrastructure/infrastructure.module';
+import { ProfileController } from './presentation';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [ProfileController],
+  imports: [InfrastructureModule],
+  providers: [
+    {
+      provide: ProfileExistenceService,
+      useFactory: (repository: IProfileRepository) =>
+        new ProfileExistenceService(repository),
+      inject: [PROFILE_REPOSITORY],
+    } satisfies Provider<ProfileExistenceService>,
+
+    {
+      provide: ApplicationService,
+      useFactory: (
+        eventPublisher: IProfileEventPublisher,
+        repository: IProfileRepository,
+        factory: IProfileFactory,
+        service: ProfileExistenceService,
+      ) => new ApplicationService(eventPublisher, repository, factory, service),
+      inject: [
+        ProfileEventBus,
+        PROFILE_REPOSITORY,
+        PROFILE_FACTORY,
+        ProfileExistenceService,
+      ],
+    } satisfies Provider<ApplicationService>,
+
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    } satisfies Provider<ValidationPipe>,
+  ],
 })
 export class AppModule {}
