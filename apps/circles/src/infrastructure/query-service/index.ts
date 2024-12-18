07040fc941aa9ddc56e833ebe744c60df892a8dd
m10maeda <mio.maeda.kari@gmail.com>
2024-12-18 18:44:14 +0900

    feat(circles): implement in memory circle data access

--- apps/circles/src/infrastructure/query-service/index.ts
+++ apps/circles/src/infrastructure/query-service/index.ts
@@ -3,3 +3,5 @@ export * from './find-all-circles.query-service';
 export * from './get-candidates.query-service';
 
 export * from './circle-data-access';
+
+export * from './in-memory-circle-data-store';
