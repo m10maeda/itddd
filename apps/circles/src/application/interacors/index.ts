879ba3a8fe0c89bfb60a8fba1871f73316517fe2
m10maeda <mio.maeda.kari@gmail.com>
2024-12-26 18:39:57 +0900

    feat(circles): implement delete relations use case input port

--- apps/circles/src/application/interacors/index.ts
+++ apps/circles/src/application/interacors/index.ts
@@ -4,3 +4,4 @@ export * from './delete-circle.interactor';
 export * from './add-member.interactor';
 export * from './remove-member.interactor';
 export * from './change-owner.interactor';
+export * from './delete-relations.interactor';
