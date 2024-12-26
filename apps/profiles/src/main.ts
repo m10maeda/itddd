d9b283151e0e5a8da624dff8c3a690e5c54559d1
m10maeda <mio.maeda.kari@gmail.com>
2024-12-26 19:07:05 +0900

    fix(profiles): make it possible to create initial topics

--- apps/profiles/src/main.ts
+++ apps/profiles/src/main.ts
@@ -2,12 +2,42 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { KafkaOptions, Transport } from '@nestjs/microservices';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { Partitioners } from 'kafkajs';
+import { Kafka, Partitioners } from 'kafkajs';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const kafka = new Kafka({
+    clientId: 'profiles',
+    brokers: [process.env.KAFKA_BROKER_URL ?? 'localhost:9094'],
+  });
+  const admin = kafka.admin();
+  const topics = await admin.listTopics();
+
+  const topicList = [];
+  if (!topics.includes('profiles')) {
+    topicList.push({
+      topic: 'profiles',
+      numPartitions: 10,
+      replicationFactor: 1,
+    });
+  }
+
+  if (!topics.includes('profiles.reply')) {
+    topicList.push({
+      topic: 'profiles.reply',
+      numPartitions: 10,
+      replicationFactor: 1,
+    });
+  }
+
+  if (topicList.length) {
+    await admin.createTopics({
+      topics: topicList,
+    });
+  }
 
   app.connectMicroservice<KafkaOptions>({
     transport: Transport.KAFKA,
