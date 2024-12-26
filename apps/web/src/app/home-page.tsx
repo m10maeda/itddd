e9453bc4bdac87b8c0f83312172c2adb57411458
m10maeda <mio.maeda.kari@gmail.com>
2024-12-26 19:09:13 +0900

    feat(web): add dashboard page

--- apps/web/src/app/home-page.tsx
+++ apps/web/src/app/home-page.tsx
@@ -1,5 +1,32 @@
-import { type PropsWithChildren } from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
-export function HomePage({ children }: PropsWithChildren) {
-  return <>{children}</>;
+import { CircleList } from './circles/circle-list';
+import { UserList } from './users/user-list';
+
+export function HomePage() {
+  return (
+    <main>
+      <Stack spacing={2}>
+        <Typography variant="h4" gutterBottom component="h1">
+          Dashboard
+        </Typography>
+
+        <Box>
+          <Typography variant="h5" gutterBottom component="h2">
+            User List
+          </Typography>
+          <UserList />
+        </Box>
+
+        <Box>
+          <Typography variant="h5" gutterBottom component="h2">
+            Circle List
+          </Typography>
+          <CircleList />
+        </Box>
+      </Stack>
+    </main>
+  );
 }
