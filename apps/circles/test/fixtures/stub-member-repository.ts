879fda5b711ed9f9761da27f78e8fc5f92299342
m10maeda <mio.maeda.kari@gmail.com>
2025-06-06 21:52:32 +0900

    test(profiles,circles): remove integration tests

--- apps/circles/test/fixtures/stub-member-repository.ts
+++
@@ -1,7 +0,0 @@
-import { createMock } from '@golevelup/ts-jest';
-
-import { IMemberExistenceService } from '../../src/domain/models/member';
-
-export const stubMemberRepository = createMock<IMemberExistenceService>({
-  exists: () => Promise.resolve(true),
-});
