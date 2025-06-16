e4fa849b4cd66ac1a33bd09c96bf356be57f4602
m10maeda <mio.maeda.kari@gmail.com>
2025-06-16 12:12:19 +0900

    refactor(circles): remove member from circle

--- apps/circles/src/domain/models/circle/circle-repository.ts
+++ apps/circles/src/domain/models/circle/circle-repository.ts
@@ -1,10 +1,8 @@
 import { type Circle } from './circle';
-import { ICircleSpecification } from './circle.specification';
 import { type CircleId } from './id/circle-id';
 import { type CircleName } from './name/circle-name';
 
 export interface ICircleRepository {
-  findAllBy(criteria: ICircleSpecification): Promise<Iterable<Circle>>;
   getBy(id: CircleId): Promise<Circle | undefined>;
   getBy(name: CircleName): Promise<Circle | undefined>;
 }
--- apps/circles/src/domain/models/circle/specifications/no-members-circle.specification.ts
+++
@@ -1,9 +0,0 @@
-import { CircleSpecification } from '../circle.specification';
-
-import type { Circle } from '../circle';
-
-export class NoMembersCircleSpecification extends CircleSpecification {
-  public isSatisfiedBy(target: Circle): boolean {
-    return target.membersCount === 0;
-  }
-}
